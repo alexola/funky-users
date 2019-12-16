ed873f612ce95745b8f5c4550b6be34bbf756499
alexo <alexola.22@gmail.com>
2019-12-16 14:26:53 +0100

    gender optimized

--- funky-react/src/App.js
+++ funky-react/src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import Navbar from './Components/Navbar';
 import Oompas from './Components/Oompas';
+// import Profile from './Components/Profile';
 import axios from 'axios';
 import './main.css';
 
 function App() {
+
   return (
     <div className="App">
       <Navbar />
+      <br />
       <Oompas />
+      {/* <Profile /> */}
     </div>
   );
 }
--- funky-react/src/Components/Oompas.js
+++ funky-react/src/Components/Oompas.js
@@ -4,23 +4,36 @@ import axios from 'axios';
 
 function Oompas() {
     const [data, setData] = useState({ results: [] });
+    
     useEffect(() => {
       const fetchData = async () => {
         const result = await axios(
-          'https://2q2woep105.execute-api.eu-west-1.amazonaws.com/napptilus/oompa-loompas?page=1',
+          'https://2q2woep105.execute-api.eu-west-1.amazonaws.com/napptilus/oompa-loompas?',
         );
         setData(result.data);
       };
       fetchData();
     }, []);
+
+    const isGender = (gender) => {
+        if (gender === "F"){
+            return 'Female'
+        } else if (gender === "M"){
+            return 'Male'
+        } else {
+            console.error('No gender')
+        }
+    }
+ 
     return (
+
+        
       <ul className="item">
         {data.results.map(item => (
           <li key={item.id}>
             <img className="photo" src={item.image} />
-            <a href={item.url}></a>
             <h3 className="fullName">{item.first_name} {item.last_name}</h3>
-            <p className="gender">{item.gender}</p>
+            <p className="gender">{isGender (item.gender)}</p>
             <p className="profession">{item.profession}</p>
           </li>
         ))}
