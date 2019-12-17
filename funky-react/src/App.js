ea1eaf5c51888c0736bd4988d414c18fbf1278e0
alexo <alexola.22@gmail.com>
2019-12-17 14:55:35 +0100

    New styles plus route working correctly

--- funky-react/src/App.js
+++ funky-react/src/App.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import Navbar from './Components/Navbar';
 import Oompas from './Components/Oompas';
-// import Profile from './Components/Profile';
-import axios from 'axios';
 import './main.css';
 
 function App() {
 
   return (
-    <div className="App">
-      <Navbar />
-      <br />
-      <Oompas />
-      {/* <Profile /> */}
-    </div>
+        <div className="App">
+          <Navbar />
+          <Oompas />
+        </div>
   );
 }
 
--- funky-react/src/Components/Oompas.js
+++ funky-react/src/Components/Oompas.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter , Route, Link, Switch } from "react-router-dom";
+
 
 
 function Oompas() {
@@ -26,21 +27,58 @@ function Oompas() {
         }
     }
 
+    const OompasPage = () => {
+      return (
+        <>
+          {data.results.map(( index) => (
+            <h5 key={index}></h5>
+          ))}
+        </>
+      );
+    };
+
+    const UserOompa = ({ match }) => {
+      const {
+        params:{ oompaId }
+      } = match
+      return (
+        <div className="profileInfo">
+          <div className="userPhoto">
+            <img alt="oompa-profile"src= {data.results[oompaId -1].image} />
+          </div>
+          <div className="userInfo">
+            <h3 className="fullName">
+              {data.results[oompaId -1].first_name} {data.results[oompaId -1].last_name}
+            </h3>
+            <p className="gender">{isGender (data.results[oompaId -1].gender)}</p>
+            <p className="profession">{data.results[oompaId -1].profession}</p>
+            <p className="userSong">{data.results[oompaId -1].favorite.song}</p>
+          </div>
+        </div>
+      )
+    } 
  
     return (
-
-      <Router>
-      <ul className="item">
-        {data.results.map(item => (
-          <li key={item.id}>
-            <img className="photo" src={item.image} />
-            <h3 className="fullName">{item.first_name} {item.last_name}</h3>
-            <p className="gender">{isGender (item.gender)}</p>
-            <p className="profession">{item.profession}</p>
-          </li>
-        ))}
-      </ul>
-      </Router>  
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/users" component={OompasPage} />
+        <Route exact path="/user/:oompaId" component={UserOompa} />
+        <ul className="item">
+          {data.results.map(item => (
+            <li key={item.id}>
+              <img alt="oompa-picture" className="photo" src={item.image} />
+                <h3 className="fullName">
+                <Link to={`/user/${item.id}`}>{item.first_name} {item.last_name}</Link>
+                  
+                  
+                  </h3>
+              <p className="gender">{isGender (item.gender)}</p>
+              <p className="profession">{item.profession}</p>
+            </li>
+          ))}
+        </ul>
+      </Switch>  
+    </BrowserRouter>      
     );
   }
   
